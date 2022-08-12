3860caaec91c10fcd34a951417e22986e81ca9b0
Kr-Kate <loly1995pop@gmail.com>
2022-08-12 23:08:27 +0400

    feat: trial solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if(str.length % 2 === 1) {
+    return false;
+  }
+  else{
+  let x = 100;
+  for(let i = 0; i < str.length; i++) {
+    switch(str[i]) {
+      case '(':
+        x += 2;
+        break;
+      case ')':
+        x -= 2;
+        if( x < 100) {
+          return false;
+        }
+        break;
+      case '[':
+        x *= 3;
+        break;
+      case ']':
+        x /= 3;
+        if( x < 100) {
+          return false;
+        }
+        break;
+      case '{':
+        x = x ** 2;
+        break;
+      case '}':
+        x = Math.sqrt(x);
+        if( x < 100) {
+          return false;
+        }
+        break;
+      case '|':
+        x = 1 / x;
+        break;
+    }
+    
+  }
+  if( x === 100) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 }
